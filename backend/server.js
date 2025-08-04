e67180712acd0bfe2207315736c7f214ee8b912e
Rishob22 <mondalrishob@gmail.com>
2025-08-04 20:30:28 +0530

    cors enabled for deployed backend

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,22 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const {
-  checkForAuthenticationCookie,
-} = require("./middlewares/authentication.js");
-const { createOrder, verifyPayment } = require("./controllers/payment.js");
 const allowedOrigins = [
   "http://localhost:3000",
   "https://sree-krishna-website.vercel.app",
 ];
-app.use(cors());
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication.js");
+const { createOrder, verifyPayment } = require("./controllers/payment.js");
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDb connected"))
@@ -30,7 +37,6 @@ const slotRoute = require("./routes/slot.js");
 app.use("/user", userRoute);
 app.use("/payment", paymentRoute);
 app.use("/slot", slotRoute);
-app.get("/", (req, res) => res.send("Backend is up and running"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
